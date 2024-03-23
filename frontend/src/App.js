622734fd20c67021fe99a35fc45f9d3a34219fd5
patryk-pi <patryk.piasecki1@gmail.com>
2024-03-23 15:47:57 +0100

    added Header component, changing components structure in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,16 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+
 const App = () => {
-    return <h1>Welcome to proshop</h1>;
+    return (
+        <>
+            <Header />
+            <main className="py-3">
+                <Container>
+                    <h1>Welcome To ProShop</h1>
+                </Container>
+            </main>
+        </>
+    );
 };
 export default App;
