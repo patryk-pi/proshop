70fbdfbb19e2afd056a8a16eef7903a790ccd424
patryk-pi <patryk.piasecki1@gmail.com>
2024-03-25 18:05:38 +0100

    MongoDB configuration

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./config/db.js";
 const port = process.env.PORT || 8000;
 import products from "./data/products.js";
+
+connectDB(); // Connect to MongoDB
 
 const app = express();
 
